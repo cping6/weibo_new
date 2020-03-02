e6b7d3236f0c7a69199a8f2169c3b101997d476a
ping <chenchangping@foxmail.com>
2020-03-02 08:09:52 +0000

    增加 CSRF 验证

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -12,8 +12,24 @@ class UsersController extends Controller
 		return view('users.create');
     }
 
+	/**
+	 * 显示信息
+	 * @param User $user
+	 * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+	 */
 	public function show(User $user)
 	{
 		return view('users.show', compact('user'));
+    }
+
+	public function store(Request $request)
+	{
+		$this->validate($request, [
+			'name' => 'required|unique:users|max:50',
+			'email' => 'required|email|unique:users|max:255',
+			'password' => 'required|confirmed|min:6'
+		]);
+
+		return ;
     }
 }
