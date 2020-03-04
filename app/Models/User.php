1654b55e770370f1db61f55a4feab5b81fb3e41d
ping <chenchangping@foxmail.com>
2020-03-04 13:51:07 +0000

    粉丝数据表

--- app/Models/User.php
+++ app/Models/User.php
@@ -13,14 +13,6 @@ class User extends Authenticatable
 
     protected $table = 'users';
 
-//    protected $fillable = [
-//    	'name', 'email', 'password',
-//	];
-//
-//    protected $hidden = [
-//    	'password', 'remember_token',
-//	];
-
     /**
      * The attributes that are mass assignable.
      *
@@ -73,5 +65,38 @@ class User extends Authenticatable
 	{
 		return $this->statuses()
 			->orderBy('created_at', 'desc');
+	}
+
+	public function followers()
+	{
+		return $this->belongsToMany(User::class, 'followers', 'user_id', 'follower_id');
+	}
+
+	public function followings()
+	{
+		return $this->belongsToMany(User::class, 'followers', 'follower_id', 'user_id');
+	}
+
+	public function follow($user_ids)
+	{
+		if ( !is_array($user_ids) ){
+			$user_ids = compact('user_ids');
+		}
+
+		$this->followings()->sync($user_ids, false);
+	}
+
+	public function unfollow($user_ids)
+	{
+		if ( !is_array($user_ids) ){
+			$user_ids = compact('user_ids');
+		}
+
+		$this->followings()->detach($user_ids);
+	}
+
+	public function isFollowing($user_id)
+	{
+		return $this->followings()->contains($user_id);
 	}
 }
